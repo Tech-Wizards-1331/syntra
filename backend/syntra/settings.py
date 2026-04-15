389a735876242e4f16377272088aae1c3862e8c5
JiyaGovani <jiya.govani12900@gmail.com>
2026-04-15 18:13:09 +0530

    Move templates and static assets to frontend folder.

--- backend/syntra/settings.py
+++ backend/syntra/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = BASE_DIR.parent / 'frontend'
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,7 +119,7 @@ ROOT_URLCONF = 'syntra.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [FRONTEND_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,7 +134,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'syntra.wsgi.application'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    FRONTEND_DIR / 'static',
 ]
 
 # Database
